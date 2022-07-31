12a513a1b2cdddf1ef2f1d39600e0534085c1396
Carl <cindymadewhat@gmail.com>
2022-07-30 20:58:11 -0500

    finished assignment

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+// const { addDestinationInfo } = require("./scriptHelper");
 
-   let listedPlanets;
+window.addEventListener("load", function() {
+    // window.alert("All fields are required.");
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+    // FIXME: MAtt added this back in
+    event.preventDefault();
+    
+    let pilot = document.querySelector("input[name=pilotName]").value;
+    let copilot = document.querySelector("input[name=copilotName]").value;
+    let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+    let cargoMass = document.querySelector("input[name=cargoMass]").value;
+    
+    formSubmission(document, pilot, copilot, fuelLevel, cargoMass);
+      })
+
+      let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+       let planet = pickPlanet(listedPlanets);
+       addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
    })
-   
+//    event.preventDefault();
 });
